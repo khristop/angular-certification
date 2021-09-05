56f7da2cf9d6b12e299fc5e6d6dea3c00f0932e5
Kevin Garcia <kgarci@acxiom.com>
2021-09-05 11:43:43 -0600

    :sparkles: Add refresh interval

--- src/app/main/weather.service.ts
+++ src/app/main/weather.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject, merge, combineLatest, of } from "rxjs";
+import { Injectable, OnDestroy } from "@angular/core";
+import { BehaviorSubject, of, interval, Subscription, forkJoin, concat } from "rxjs";
+import { startWith } from "rxjs/operators";
 import { WeatherResponse } from "../core/models/weather-api.model";
 import { Weather } from "../core/models/weather.model";
 import { StorageService } from "../core/services/storage.service";
 import { WeatherAPIService } from "../core/services/weather-api.service";
 
 @Injectable()
-export class WeatherService {
+export class WeatherService implements OnDestroy {
   private locationsKey = "locations";
   private zipcodes = this.getLocations();
+  private refreshInterval = 30000;
+  private refreshSubscription: Subscription;
 
   private locationWeathersSubject$ = new BehaviorSubject<Weather[]>([]);
   private get locationWeathers() {
@@ -23,23 +26,39 @@ export class WeatherService {
     this.fetchData();
   }
 
-  protected fetchData() {
-    if (this.zipcodes) {
-      const observers = this.zipcodes.map(zipcode =>
-        combineLatest([
-          of(zipcode),
-          this.weatherAPIService.getWeatherByZipcode(zipcode)
-        ])
-      );
+  ngOnDestroy() {
+    this.refreshSubscription.unsubscribe();
+  }
 
-      merge(...observers).subscribe(([zipcode, weatherData]) => {
-        weatherData["zipcode"] = zipcode;
-        this.locationWeathersSubject$.next([
-          ...this.locationWeathers,
-          weatherData as Weather
-        ]);
+  protected fetchData() {
+    this.refreshSubscription = interval(this.refreshInterval)
+      .pipe(startWith(0))
+      .subscribe( () => {
+        if (this.zipcodes?.length > 0) {
+          const observers = this.zipcodes.map(zipcode =>
+            forkJoin([
+              of(zipcode),
+              this.weatherAPIService.getWeatherByZipcode(zipcode)
+            ])
+          );
+    
+          concat(...observers).subscribe(([zipcode, weatherResponse]) => {
+            const newWeatherData = {
+              ...weatherResponse,
+              zipcode
+            };
+            const oldWeatherData = [...this.locationWeathersSubject$.value];
+            let updateWeather = [];
+
+            if (oldWeatherData.length > 0 && oldWeatherData.find(weather => weather.zipcode === zipcode)) {
+              updateWeather = oldWeatherData.map(weather => weather.zipcode === zipcode ? newWeatherData : weather);
+            } else {
+              updateWeather = [...oldWeatherData, newWeatherData];
+            }
+            this.locationWeathersSubject$.next(updateWeather);
+          });
+        }
       });
-    }
   }
 
   private saveLocations(locations: string[]): void {
